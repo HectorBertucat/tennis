9b5b109288eed4668a9c175acb117dbdef9dccd0
hectorbertucat <hectorbertucat@gmail.com>
2025-10-20 11:42:42 +0200

    Added real logic for healthcheck with active connections

--- src/main/java/fr/hectorb/tennis/service/HealthCheckService.java
+++ src/main/java/fr/hectorb/tennis/service/HealthCheckService.java
@@ -2,12 +2,23 @@ package fr.hectorb.tennis.service;
 
 import fr.hectorb.tennis.ApplicationStatus;
 import fr.hectorb.tennis.HealthCheck;
+import fr.hectorb.tennis.repository.HealthCheckRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HealthCheckService {
 
+    @Autowired
+    private HealthCheckRepository healthCheckRepository;
+
     public HealthCheck healthCheck() {
-        return new HealthCheck(ApplicationStatus.OK, "Welcome to Hector tennis");
+        Long nbDbConnections = healthCheckRepository.countApplicationConnections();
+
+        if (nbDbConnections > 0) {
+            return new HealthCheck(ApplicationStatus.OK, "Db has " + nbDbConnections + " connections");
+        }  else {
+            return new HealthCheck(ApplicationStatus.KO, "Db has " + nbDbConnections + " connections");
+        }
     }
 }
