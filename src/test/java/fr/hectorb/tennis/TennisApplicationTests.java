360409801ae5c645c5eb1f661fa7dbe4c50a44d4
hectorbertucat <hectorbertucat@gmail.com>
2025-10-27 10:03:50 +0100

    feat: add basic tests to check if HelathCheck service, controller and repo are loaded

--- src/test/java/fr/hectorb/tennis/TennisApplicationTests.java
+++ src/test/java/fr/hectorb/tennis/TennisApplicationTests.java
@@ -1,13 +1,28 @@
 package fr.hectorb.tennis;
 
+import fr.hectorb.tennis.repository.HealthCheckRepository;
+import fr.hectorb.tennis.service.HealthCheckService;
+import fr.hectorb.tennis.web.HealthCheckController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TennisApplicationTests {
 
+    @Autowired
+    private HealthCheckController healthCheckController;
+    @Autowired
+    private HealthCheckRepository healthCheckRepository;
+    @Autowired
+    private HealthCheckService healthCheckService;
+
 	@Test
 	void contextLoads() {
+        Assertions.assertThat(healthCheckController).isNotNull();
+        Assertions.assertThat(healthCheckRepository).isNotNull();
+        Assertions.assertThat(healthCheckService).isNotNull();
 	}
 
 }
